7f7cff2e2cbec885446ba50a0f9e94a8e0dd3d3c
seth411 <100421639+seth411@users.noreply.github.com>
2022-04-06 08:30:53 -0400

    Update student.py

--- student.py
+++ student.py
@@ -9,8 +9,8 @@ def main():
     elif (number%5) == 0:
       print("buzz")
 
-    elif (number%3) and (number%5) == 0:
-      print("fizzbuzz")
+    elif (number%3) == 0:
+      print("fizz")
 
     else:
       print(number)
